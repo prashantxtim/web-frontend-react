c58609fcf94ea3bd43976500837fecce2cd1dd94
prashantxtim <prashantxtim@gmail.com>
2023-03-01 22:42:00 +0545

    helmet in home

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -5,5 +5,6 @@ const Helmet = (props) => {
   document.title = "FreelanceHQ || " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
+// this is helmet
 
 export default Helmet;
